bc118c9c0135807dd208be118d2f68dc724db632
flamey18 <include.prares+github@gmail.com>
2023-05-10 12:05:12 +0300

    Chestii
    fundal pentru furtuna si ninsoare,schimbare culoare text in functie de ora

--- node_2d.gd
+++ node_2d.gd
@@ -12,33 +12,35 @@ func _ready():
 	await get_tree().create_timer(4).timeout
 	codul_vremii()
 	if(meteo["current_weather"]["is_day"]==1):
+		$Text/StareaVremii.set("theme_override_colors/font_color",Color(0,0,0))
+		$Text/Bacau.set("theme_override_colors/font_color",Color(0,0,0))
+		$Text/Temperatura.set("theme_override_colors/font_color",Color(0,0,0))
 		match (vreme):
 			0:
-				$Day.show()
 				$Day/Clear.show()
 			1:
-				$Day.show()
 				$Day/Cloudy.show()
 			2:
-				$Day.show()
 				$Day/Rainy.show()
 			3:
-				$Day.show()
-				$Day/Cloudy.show()
+				$Day/Snowy.show()
+			4:
+				$Day/Thunderstorm.show()
 	else:
+		$Text/StareaVremii.set("theme_override_colors/font_color",Color(255,255,255))
+		$Text/Bacau.set("theme_override_colors/font_color",Color(255,255,255))
+		$Text/Temperatura.set("theme_override_colors/font_color",Color(255,255,255))
 		match (vreme):
 			0:
-				$Night.show()
 				$Night/Clear.show()
 			1:
-				$Night.show()
 				$Night/Cloudy.show()
 			2:
-				$Night.show()
 				$Night/Rainy.show()
 			3:
-				$Night.show()
-				$Night/Cloudy.show()
+				$Night/Snowy.show()
+			4:
+				$Night/Thunderstorm.show()
 	$Text/Temperatura.text=str(meteo["current_weather"]["temperature"])+temp
 #Cand cererea HTTP este completa,copiaza toate datele primite in variabila meteo
 #Variabila meteo este un dictionar,un fel de vector in C++
@@ -47,27 +49,29 @@ func _on_request_completed(result, response_code, headers, body):
 	meteo = JSON.parse_string(body.get_string_from_utf8())
 #Fiecare tip de vreme are codul sau specific
 #Aceasta functie va decoda codul vremii
-#0-senin 1-innorat 2-plaoie 3-ninsoare
+#0-senin 1-innorat 2-plaoie 3-ninsoare 4-furtuna
 func codul_vremii():
 	if(meteo):
 		cod=int(meteo["current_weather"]["weathercode"])
 		match (cod):
 			0,1,2,3:
-				des="In principal senin"
+				$Text/StareaVremii.text="In principal senin"
 				vreme=0
 			45,48:
-				des="Ceata"
+				$Text/StareaVremii.text="Ceata"
 				vreme=1
 			51,53,55,56,57:
-				des="Burnita"
+				$Text/StareaVremii.text="Burnita"
 				vreme=1
 			61,63,65,66,67:
-				des="Ploaie usoara"
+				$Text/StareaVremii.text="Ploaie usoara"
 				vreme=2
-			71,73,75,77:
-				des="Ninsoare"
+			71,73,75,77,85,86:
+				$Text/StareaVremii.text="Ninsoare"
 				vreme=3
 			80,81,82:
-				des="Averse de ploaie"
-				vreme=3
-		$Text/StareaVremii.text=des
+				$Text/StareaVremii.text="Averse de ploaie"
+				vreme=2
+			95,96,97:
+				$Text/StareaVremii.text="Furtuna"
+				vreme=4
